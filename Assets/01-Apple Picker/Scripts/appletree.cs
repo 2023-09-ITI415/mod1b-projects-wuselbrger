fe00b587796b3ee6c12a620a8964a98f9e695d55
wuselbrger <kevinkwu59@gmail.com>
2023-09-28 20:02:55 -0400

    commit-3
    updated appletree script

--- Assets/01-Apple Picker/Scripts/appletree.cs
+++ Assets/01-Apple Picker/Scripts/appletree.cs
@@ -4,15 +4,38 @@ using UnityEngine;
 
 public class appletree : MonoBehaviour
 {
+    public GameObject applePrefab;
+    public float speed = 1.0f;
+    public float leftandRightEdge = 10f;
+    public float chanceToChangeDirections = 0.1f;
+    public float secondsBetweenAppleDrops = 1f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Invoke("DropApple", 2f);   
     }
 
     // Update is called once per frame
     void Update()
     {
+        Vector3 pos = transform.position;
+        pos.x += speed * Time.deltaTime;
+        transform.position = pos;
+        if(pos.x < -leftandRightEdge)
+            speed = Mathf.Abs(speed);
+        else if(pos.x > leftandRightEdge)
+            speed = -Mathf.Abs(speed);
+    }
+
+    void FixedUpdate(){
+        if(Random.value < chanceToChangeDirections)
+            speed *= -1;
         
+    }
+
+    void DropApple(){
+        GameObject apple = Instantiate<GameObject>(applePrefab);
+        apple.transform.position = transform.position;
+        Invoke("DropApple",secondsBetweenAppleDrops);
     }
 }
